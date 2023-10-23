7431f7e47b505ec5eed71d7314d6cd9437f44dc9
logon84 <logon84@users.noreply.github.com>
2023-10-23 22:33:44 +0200

    Update boot_inactivity_shutdown.sh

--- boot_inactivity_shutdown.sh
+++ boot_inactivity_shutdown.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-if [ $(sleep 600 && xprintidle) -gt "598000" ]; then shutdown -P now; fi
+#This script requires xprintidle and enhanced notify-send (https://github.com/vlevit/notify-send.sh)
+if [ $(sleep 600 && xprintidle) -gt "598000" ]
+then
+    countdown=30
+	ID=$(notify-send.sh Autoshutdown -t 1 "Shutting down in $countdown seconds" -p)
+	while [ $(xprintidle) -gt "598000" ]
+	do
+		countdown=$((countdown-1))
+		sleep 1
+		$(notify-send.sh Autoshutdown -r $ID -t 1 "Shutting down in $countdown seconds")
+		if [ $countdown -eq "0" ]
+		then
+			shutdown -P now
+		fi
+	done
+fi
