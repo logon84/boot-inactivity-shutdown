071504bfc583389c94b215b24f89610de9379add
logon84 <logon84@users.noreply.github.com>
2025-02-21 15:22:51 +0100

    Update boot_inactivity_shutdown.sh

--- boot_inactivity_shutdown.sh
+++ boot_inactivity_shutdown.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
-#This script requires xprintidle and enhanced notify-send (https://github.com/vlevit/notify-send.sh)
-if [ $(sleep 600 && xprintidle) -gt "598000" ]
+#This script requires xprintidle.
+timeout_secs=600
+
+if [ $(sleep $timeout_secs && xprintidle) -gt $((1000*(timeout_secs-2))) ]
 then
+	echo "0" > /tmp/bis.id
 	countdown=30
-	ID=$(notify-send.sh Autoshutdown -t 1 "Shutting down in $countdown seconds" -p)
-	while [ $(xprintidle) -gt "598000" ]
+	while [ $(xprintidle) -gt $((1000*(timeout_secs-2))) ]
 	do
-		countdown=$((countdown-1))
-		sleep 1
-		$(notify-send.sh Autoshutdown -r $ID -t 1 "Shutting down in $countdown seconds")
+		$(gdbus call --session \
+		--dest org.freedesktop.Notifications \
+		--object-path /org/freedesktop/Notifications \
+		--method org.freedesktop.Notifications.Notify sss \
+		"$(</tmp/bis.id)" \
+		gtk-dialog-info "Boot Inactivity Shutdown" "Shutting down in $countdown seconds" [] {} 1000 | \
+		sed 's/(uint32 \([0-9]\+\),)/\1/g' > /tmp/bis.id)
 		if [ $countdown -eq "0" ]
 		then
 			shutdown -P now
+			break
 		fi
+		sleep 1
+		countdown=$((countdown-1))
 	done
 fi
