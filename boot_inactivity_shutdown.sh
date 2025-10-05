cd22457865361ee57c3c206f4870da3d5340a5b6
logon84 <logon84@users.noreply.github.com>
2025-10-05 21:16:42 +0200

    Update boot_inactivity_shutdown.sh

--- boot_inactivity_shutdown.sh
+++ boot_inactivity_shutdown.sh
@@ -7,18 +7,21 @@ echo "0" > /tmp/bis.id
 sleep $timeout_secs
 while [ $(xprintidle) -gt $((1000*(timeout_secs-2))) ]
 do
-	countdown=$((countdown-1))
-	$(gdbus call --session \
-	--dest org.freedesktop.Notifications \
-	--object-path /org/freedesktop/Notifications \
-	--method org.freedesktop.Notifications.Notify bis \
-	"$(</tmp/bis.id)" \
-	gtk-dialog-info "Boot Inactivity Shutdown" "Shutting down in $countdown seconds" [] {} 1000 | \
-	sed 's/(uint32 \([0-9]\+\),)/\1/g' > /tmp/bis.id)
-	sleep 1
-	if [ $countdown -eq "0" ]
-	then
-		shutdown -P now
-		break
-	fi
+    countdown=$((countdown-1))
+    $(gdbus call --session \
+    --dest org.freedesktop.Notifications \
+    --object-path /org/freedesktop/Notifications \
+    --method org.freedesktop.Notifications.Notify bis \
+    "$(</tmp/bis.id)" \
+    gtk-dialog-info "Boot Inactivity Shutdown" "Shutting down in $countdown seconds" [] {} 1000 | \
+    sed 's/(uint32 \([0-9]\+\),)/\1/g' > /tmp/bis.id)
+    sleep 1
+    if [ $countdown -eq "0" ]
+    then
+        while true
+        do
+            shutdown -P now
+            sleep 30
+        done
+    fi
 done
